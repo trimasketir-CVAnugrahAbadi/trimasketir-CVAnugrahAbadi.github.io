3c26e24c2594c5954551e405c62740327afd5ae5
luckysetiawan <luckysetiawan0802@gmail.com>
2023-11-26 14:38:17 +0700

    refactor catalogue and added image support to product data

---
+++ js/catalogue.js
@@ -0,0 +1,59 @@
+const getCard = (product) => {
+    return`
+        <div class="col">
+            <div class="card mt-2 mb-2" style="width: 18rem;">
+                <img src="img/products/${product.image}" class="card-img-top" alt="${product.image}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">Rp. ${product.price}</p>
+                    <a href="order.html" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+const getCatalogue = (productData) => {
+    const fillerCards = `
+        <div class="col">
+            <div class="card mt-2 mb-2" style="width: 18rem; visibility: hidden;"></div>
+        </div>
+    `;
+
+    let catalogueCards = `<div class="container">`;
+
+    for (let i = 0; i < productData.length; i += 4) {
+        catalogueCards += `<div class="row">`;
+
+        if(productData.hasOwnProperty(i)) {
+            catalogueCards += getCard(productData[i]);
+        } else {
+            catalogueCards += fillerCards;
+        }
+
+        if(productData.hasOwnProperty(i+1)) {
+            catalogueCards += getCard(productData[i+1]);
+        } else {
+            catalogueCards += fillerCards;
+        }
+
+        if(productData.hasOwnProperty(i+2)) {
+            catalogueCards += getCard(productData[i+2]);
+        } else {
+            catalogueCards += fillerCards;
+        }
+
+        if(productData.hasOwnProperty(i+3)) {
+            catalogueCards += getCard(productData[i+3]);
+        } else {
+            catalogueCards += fillerCards;
+        }
+
+        catalogueCards += `</div>`;
+
+    }
+
+    catalogueCards += `</div>`;
+
+    return catalogueCards;
+}
